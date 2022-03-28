7cb286fcdf7828e028bba658e5a73cc5c2e92ad2
Nikola Laskov <nikola@laskov.us>
2022-03-29 01:51:34 +0300

    First Model(Database) commit

---
+++ The Time Project/The Time Project/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  The Time Project
+//
+//  Created by Nikola Laskov on 29.03.22.
+//
+
+import Foundation
+
+class User{
+    var name:String = ""
+    var UID:String = ""
+    var email:String = ""
+    var image:String = ""
+}
+
+/* Image да бъде името на файла който се пази в firebaseStorage */
